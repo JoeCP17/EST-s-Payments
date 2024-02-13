184487707f6e9665da794a8d278916dff40dccc4
Ueibin Kim <at365@naver.com>
2024-02-13 21:12:31 +0900

    setting : Setting Projects

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "EST-s-Payments"
---
+++ src/test/kotlin/com/example/estdelivery/EstPaymentsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.estdelivery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EstPaymentsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
